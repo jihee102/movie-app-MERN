57613c915b4654dbb5fc91faf077023a1e48bbdc
Jihee Hong <486480@student.saxion.nl>
2021-02-11 11:44:24 +0100

    favorite btn

--- client/src/components/views/MovieDetailPage/section/Favorite.js
+++ client/src/components/views/MovieDetailPage/section/Favorite.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FAVORITE_SERVER } from '../../../Config';
 import axios from 'axios';
 
@@ -8,6 +8,9 @@ function Favorite(props) {
   const movieTitle = props.movie.title;
   const moviePost = props.movie.backdrop_path;
   const movieRunTime = props.movie.rundtime;
+
+  const [FavoriteNumber, setFavoriteNumber] = useState(0);
+  const [Favorited, setFavorited] = useState(false);
 
   useEffect(() => {
     let variables = {
@@ -19,16 +22,30 @@ function Favorite(props) {
       .post(`${FAVORITE_SERVER}/favorite-number`, variables)
       .then((response) => {
         if (response.data.success) {
-          console.log(response.data.favoriteNumber);
+          setFavoriteNumber(response.data.favoriteNumber);
         } else {
           alert('Fail to get the favorite number');
         }
       });
+
+    axios.post(`${FAVORITE_SERVER}/favorited`, variables).then((response) => {
+      setFavorited(response.data.favorited);
+      if (!response.data.success) {
+        alert('Fail to load favoried information');
+      }
+    });
   }, []);
+
+  const toggleFavorite = () => {
+    setFavorited(!Favorited);
+  };
 
   return (
     <div>
-      <button>Favorite</button>
+      <button onClick={toggleFavorite}>
+        {Favorited ? 'Delete Favorite ' : 'Add Favorite '}
+        {FavoriteNumber}
+      </button>
     </div>
   );
 }
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -15,4 +15,18 @@ router.post('/favorite-number', (req, res) => {
   });
 });
 
+router.post('/favorited', (req, res) => {
+  const movieId = req.body.movieId;
+
+  Favorite.find({ movieId: movieId, user: req.body.user }).exec((err, data) => {
+    if (err) return res.status(400).send(err);
+
+    let result = false;
+    if (data.length !== 0) {
+      result = true;
+    }
+    res.status(200).json({ success: true, Favorited: result });
+  });
+});
+
 module.exports = router;
